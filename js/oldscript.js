f02e78fffad7b42679473d2b610bc4d3d2e14277
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2019-04-14 16:08:08 +1200

    Third Commit
    STABLE RELEASE It just works.
    
    Decided to scale back the animation spinner as its a bit outside of my expertise at the moment. I may come back to this once I've got more experience in bootstrap animation. Otherwise, I'm happy with it.

--- js/oldscript.js
+++ js/oldscript.js
@@ -5,7 +5,12 @@ function Team(name, logo, message) {
     this.message = message;
 }
 
-var defaultTeam = {name: "", logo: "img/UnknownTeam.png", message: ""}
+// //declare array of team objects.
+// var teamArray = new Array();
+
+var testTeam = {name: "TESTNAME", logo: "tESTIMG", message: "TESTDESC"}
+
+
 var pats = {name: "New England Patriots", logo: "img/pats.png", message: "Tom Brady: The Team."}
 var cowboys = {name: "Dallas Cowboys", logo: "img/cowb.png", message: "An elite team with a sketchy owner."}
 var eagles = {name: "Philadelphia Eagles", logo: "img/eagles.jpg", message: "Good Team, Obnoxious Fans"}
@@ -16,7 +21,7 @@ var raiders =  { name: "Oakland Raiders", logo: "img/raid.jpg", message: "Yarr H
 var chicagoBears = { name: "Chicago Bears", logo: "img/bears.jpg", message: "Wait, aren't they a baseball team?"}
 var norlSaints =  { name: "New Orleans Saints", logo: "img/saints.png", message: "Yo why is everything from N'Orleans deep fried?"}
 var laRams =  { name: "Los Angeles Rams", logo: "img/rams.jpg", message: "Recently destroyed by the Patriots"}
-var kansaCityChiefs =  { name: "Kansas City Chiefs", logo: "img/chief.jpg", message: "America's least racist Native American themed team."}
+var kansaCityChiefs =  { name: "Kansas City Chiefs", logo: "img/chiefs.jpg", message: "America's least racist Native American themed team."}
 var nyGiants =  { name: "New York Giants", logo: "img/nyg.jpg", message: "They're not that big, really."}
 var sanFran49ers =  { name: "San Francisco 49ers", logo: "img/sf49.jpg", message: "HUR HUR MORE LIKE 69ERS"}
 var washingtonRedskins =  { name: "Washington Redskins", logo: "img/wrs.jpg", message: "Bruh just change your name already."}
@@ -28,38 +33,105 @@ var nyJets =  {name: "New York Jets", logo: "img/nyj.jpg", message: "Gee New Yor
 var houstTex =  {name: "Houston Texans", logo: "img/houst.jpg", message: "Rivals to to the Jacksonville Mississipians and the Chicago Illinoisans"}
 var detroitLions =  {name: "Detroit Lions", logo: "img/lions.jpg", message: "The players are just happy to have jobs, really."}
 var indianapolisColts =  {name: "Indianapolis Colts", logo: "img/colt.jpg", message: "They have a Colt following"}
-var laChargers =  {name: "Los Angeles Chargers", logo: "img/charg.jpg", message: "Their logo looks like a Schoolboy's haircut."}
+var laChargers =  {name: "Los Angeles Chargers", logo: "img/charg.png", message: "Their logo looks like a Schoolboy's haircut."}
 var carolinaPanthers =  {name: "Carolina Panthers", logo: "img/panthers.jpg", message: "Who cares"}
 var atlantaFalcons =  {name: "Atlanta Falcons", logo: "img/falcons.jpg", message: "cool I guess"}
 var cincinBengals =  {name: "Cincinatti Bengals", logo: "img/bengal.jpg", message: "Someone needs to tell them that a Bengal is a place, not just a Tiger."}
-var buffaloBills =  {name: "Buffalo Bills", logo: "img/bills.jpg", message: "It puts the lotion in the basket or else it gets the hose again."}
+var buffaloBills =  {name: "Buffalo Bills", logo: "img/bill.jpg", message: "None of the players are actually called Bill."}
 var tenTitans =  {name: "Tennessee Titans", logo: "img/titans.jpg", message: "The name 'Giants' was taken."}
 var jacksonJag =  {name: "Jacksonville Jaguars", logo: "img/jag.jpg", message: "They're based in London. Naturally."}
 var arizonaCardinals =  {name: "Arizona Cardinals", logo: "img/card.png", message: "Not eligible for the Papacy."}
 var tampBuc =  {name: "Tampa Bay Buccanners", logo: "img/bucc.jpg", message: "Totally forgot about this team until the last minute."}
 var browns =  {name: "Cleveland Browns", logo: "img/browns.jpg", message: "Move over Detroit, there's a new Factory of Sadness"}
 
+var sampleTeamArray = [pats, cowboys, eagles, gbPackers, steelers, broncos, raiders, chicagoBears];
 var fullTeamArray = [pats, cowboys, eagles, gbPackers, steelers, broncos, raiders, chicagoBears, norlSaints, laRams, kansaCityChiefs, nyGiants,
                       sanFran49ers, washingtonRedskins, seattleSeahawks, minnesotaVikings, miamiDolphins, baltimoreRaves, nyJets, houstTex, detroitLions,
                     indianapolisColts, laChargers, carolinaPanthers, atlantaFalcons, cincinBengals, buffaloBills, tenTitans, jacksonJag, tampBuc, browns];
 
-//Shows Minified version behind team.
-function slideTeam(team) {
-  return $('#logo').html("<h1>" + team.name + "</h1>"
-    + "<h4>" + team.message + "</h4>"
-    + "<div id='teamImg'><img src='" + team.logo + "' alt=''></img></div>");
-    console.log("DEFAULT2ENTERED");
+function Spinner(teamArray) {
+  this.teamList = teamArray;
+  this.teamActive = testTeam;
 }
 
-//BugTest Function. Shows all logos & names to check img path for logos.
-function showAllLogos() {
-  for(var i = 0; i < fullTeamArray.length; i++) {
-    $('#logo').append(fullTeamArray[i].name + "<img src='" + fullTeamArray[i].logo + "' alt=''></img>");
+var spin = {teamList: sampleTeamArray };
+
+//populate spinner w/ images corresponding to objects.
+function populateSpinner(spinner) {
+  for (var i = 0; i < spinner.teamList.length; i++) {
+    var build = "<div class='item'>"
+      + "<img src=" + spinner.teamList[i].logo + " alt=" + spinner.teamList[i].name + "/></div>";
+
+    document.getElementById('carousel-inner').appendChild = build;
   }
 }
 
-//ACTION FUNCTIONS
-function spinCarousel() {
-  var chosenTeam = Math.floor(Math.random() * (fullTeamArray.length - 0));
-  slideTeam(fullTeamArray[chosenTeam]);
+//create function to cycle through teams.
+
+
+//create function to display team name & logo.
+function expandTeam(team) {
+  // var wrapper = $('#wrapper'), container;
+  // container = $('<div class = "team" class="container"></div>');
+  // container.append('<div class="teamName"' + team.name + "</div>");
+  // container.append('<div class="teamLogo"><img src="' + team.logo + '" alt="' + team.name + '"></div>');
+  // container.append('<div class="teamDesc"' + team.message + "</div>");
+  // document.write(container);
+
+  return document.getElementById('logo').innerHTML =
+    "<img src='" + team.logo + "' alt='' id='activeTeamLogo'></img>" +
+    "<div class='carousel-caption'>" +
+      "<h3 class='h3-responsive'>" + team.name + "</h3>" +
+      "<p>" + team.message  + "</p></div>";
+};
+
+//Shows Minified version behind team.
+function slideTeam(team) {
+  return document.getElementById('activeTeam').innerHTML = "<h2>" + team.name + "</h2>"
+    + "<h3>" + team.message + "</h3>"
+    + "<img src='" + team.logo + "' alt=''></img>";
 }
+
+
+//ACTION FUNCTIONS
+
+function spinCarousel() {
+
+}
+
+// $(document).ready(function() {
+//   jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
+// });
+
+// $('.spinButton').on('click', function() {
+//   $("#carousel").carousel($(this).index());
+// })
+
+//
+// $('#carouselExample').on('slide.bs.carousel', function (e) {
+//
+//     /*
+//
+//     CC 2.0 License Iatek LLC 2018
+//     Attribution required
+//
+//     */
+//     $("#myCarousel").carousel({ interval: 500, wrap: true });
+//     var $e = $(e.relatedTarget);
+//     var idx = $e.index();
+//     var itemsPerSlide = 3;
+//     var totalItems = $('.carousel-item').length;
+//
+//     if (idx >= totalItems-(itemsPerSlide-1)) {
+//         var it = itemsPerSlide - (totalItems - idx);
+//         for (var i=0; i<it; i++) {
+//             // append slides to end
+//             if (e.direction=="left") {
+//                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
+//             }
+//             else {
+//                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
+//             }
+//         }
+//     }
+// });
